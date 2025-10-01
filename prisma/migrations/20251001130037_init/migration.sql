c16cce0dfcdd712930543f796de6aa57158e14a0
jon shutt <jon@rethinkcarbon.co.uk>
2025-10-01 13:01:15 +0100

    add migration

---
+++ prisma/migrations/20251001130037_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
+
+-- CreateTable
+CREATE TABLE "public"."Project" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
